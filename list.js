53a114b91064aad55486a7366f1f7daba283c208
Jan <jan.evseev@gmail.com>
2016-11-22 14:33:42 +0300

    add solution

--- list.js
+++ list.js
@@ -1,2 +1,117 @@
 'use strict';
 
+const people = [
+  {
+    name: 'Daenerys Targaryen',
+    gender: 'female'
+  },
+  {
+    name: 'Jon Snow',
+    gender: 'male'
+  },
+  {
+    name: 'Tyrion Lannister',
+    gender: 'male'
+  },
+  {
+    name: 'Ned Stark',
+    gender: 'male'
+  },
+  {
+    name: 'Cersei Lannister',
+    gender: 'female'
+  },
+  {
+    name: 'Sansa Stark',
+    gender: 'female'
+  },
+  {
+    name: 'Arya Stark',
+    gender: 'female'
+  },
+  {
+    name: 'Peter Baelish',
+    gender: 'male'
+  },
+  {
+    name: 'Hodor',
+    gender: 'male'
+  },
+  {
+    name: 'Margaery Tyrell',
+    gender: 'female'
+  }
+];
+
+const jobs = ['King in the North', 'Khaleesi', 'King of the Andals, the Rhoynar, and the First',
+              'Hand of the King', 'Lord Commander of the Night\'s watch', 'Khal', 'Warden',
+              'Master of Coin', 'King-Beyond-the-Wall', 'Lord of Winterfell' ];
+
+
+
+function generatePeopleArray (peopleList, jobList, resultArrayLength, minAge, maxAge, minSalary, maxSalary) {
+  let generatedArray = [];
+
+  for (let i = 0; i <= resultArrayLength; i++) {
+    let pickedPersonId = Math.floor(Math.random() * peopleList.length);
+    let generatedPerson = {
+      name: peopleList[pickedPersonId]['name'],
+      gender: peopleList[pickedPersonId]['gender'],
+      job: jobList[Math.floor(Math.random() * jobList.length)],
+      age: Math.floor((Math.random() * (maxAge - minAge + 1)) + minAge),
+      salary: Math.floor((Math.random() * (maxSalary - minSalary)) + minSalary)
+    }
+    generatedArray.push(generatedPerson);
+  }
+  return generatedArray;
+}
+
+function addStyles(elem) {
+  let {job, age, salary, gender} = elem.dataset;
+
+  elem.style.margin = '15px';
+  elem.style.width = '200px';
+  if (salary < 50) {
+    elem.style.background = 'red';
+  } else if ( salary <= 80 && salary >= 50) {
+    elem.style.background = 'yellow';
+  } else {
+    elem.style.background = 'green';
+  }
+
+  if (age <= 27 && age >= 20) {
+    elem.querySelector('.name').style.fontWeight = 700;
+  }
+
+  if (job === 'King in the North') {
+    elem.style.borderBottom = '2px solid blue';
+  }
+
+  if (gender === 'female') {
+    elem.style.fontSize = `150%`;
+  }
+}
+
+function createDomList(people) {
+  let ul = document.createElement('ul');
+  ul.style.display = 'flex';
+  ul.style.flexWrap = 'wrap';
+  ul.style.listStyle = 'none';
+
+  people.forEach(person => {
+    let li = document.createElement('li');
+    for (let prop in person) {
+      let propElement = document.createElement('p');
+      propElement.classList.add(`${prop}`);
+      li.setAttribute(`data-${prop}`, `${person[prop]}`);
+      propElement.innerHTML = `${prop}: ${person[prop]}`;
+      li.appendChild(propElement);
+    };
+    addStyles(li);
+    ul.appendChild(li);
+  });
+  document.body.appendChild(ul);
+}
+
+let peopleArray = generatePeopleArray(people, jobs, 10, 20, 35, 20, 120);
+createDomList(peopleArray);
--- time.js
+++ time.js
@@ -1,2 +1,38 @@
 'use strict';
 
+function initializeClock (date = Date.now(), format = ['hours', 'minutes', 'seconds']) {
+      let container = document.createElement('div');
+      container.style.display = 'flex';
+
+      document.body.appendChild(container);
+
+      format.forEach((dateProp) => {
+        let item = document.createElement('div');
+        let title = document.createElement('h4');
+        let value = document.createElement('span');
+        let isPropYear = dateProp === 'year';
+        
+        item.style.margin = '15px';
+        title.innerHTML = `${dateProp}`;
+        value.setAttribute(`data-function`, `get${isPropYear ? 'Full' : ''}${dateProp[0].toUpperCase() + dateProp.slice(1)}`);
+
+        item.appendChild(title);
+        item.appendChild(value);
+        container.appendChild(item);
+      });
+    updateClock();
+
+    function updateClock() {
+      [...document.getElementsByTagName('span')].forEach((dateProp) => {
+        dateProp.innerHTML = date[dateProp.dataset.function]();
+      });
+      date.setSeconds(date.getSeconds() + 1);
+    }
+
+    setInterval(updateClock, 1000);
+};
+
+initializeClock(new Date(2014, 6, 14));
+//initializeClock(['hours', 'seconds']);
+//initializeClock(new Date(2014, 6, 14), ['year', 'month', 'seconds']);
+//initializeClock();
